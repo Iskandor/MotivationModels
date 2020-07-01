0dfc948461f4abd91f49eead0cb020f0cf753a6f
pechac <higsik@gmail.com>
2020-07-01 19:07:15 +0200

    half cheetah without stats collection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 
 import DDPG_FetchReach
+import DDPG_HalfCheetah
 import DDPG_LunarLander
 import DDPG_MountainCar
 #import PPO_Go
@@ -10,7 +11,8 @@ import PPO_Pong
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Motivation models learning platform.')
 
-    parser.add_argument('--env', type=str, choices=['mountain_car', 'lunar_lander', 'fetch_reach', 'go', 'chess', 'pong'], help='[mountain_car,lunar_lander,fetch_reach,go,chess, pong]')
+    parser.add_argument('--env', type=str, choices=['half_cheetah', 'mountain_car', 'lunar_lander', 'fetch_reach', 'go', 'chess', 'pong'],
+                        help='[half_cheetah,mountain_car,lunar_lander,fetch_reach,go,chess, pong]')
     parser.add_argument('--model', type=str, choices=['baseline', 'fm', 'su'], help='[baseline,fm,su]')
     parser.add_argument('--load', type=str, help='path to saved agent')
     parser.add_argument('--trials', type=int, help='No. trials')
@@ -49,5 +51,13 @@ if __name__ == '__main__':
     if args.env == 'pong':
         if args.model == 'baseline':
             PPO_Pong.run_baseline(args)
+    if args.env == 'half_cheetah':
+        if args.model == 'baseline':
+            DDPG_HalfCheetah.run_baseline(args)
+        if args.model == 'fm':
+            DDPG_HalfCheetah.run_forward_model(args)
+        if args.model == 'su':
+            DDPG_HalfCheetah.run_metalearner_model(args)
+
     # DQN_FrozenLake.run()
     # DQN_CartPole.run()
