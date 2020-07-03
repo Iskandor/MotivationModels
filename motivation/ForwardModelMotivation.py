internal reward calculation fix

motivation/ForwardModelMotivation.py
@@ -32,14 +32,11 @@ class ForwardModelMotivation:
 
         return error
 
-    def reward(self, state0, action, state1, eta=1.0):
-        error = self.error(state0, action, state1)
-        reward = torch.tanh(error)
-
-        return reward * eta
-
-    def reward(self, error, eta=1.0):
-        reward = torch.tanh(error)
+    def reward(self, state0=None, action=None, state1=None, error=None, eta=1.0):
+        if error is None:
+            reward = torch.tanh(self.error(state0, action, state1))
+        else:
+            reward = torch.tanh(error)
 
         return reward * eta
 

motivation/MateLearnerMotivation.py
@@ -48,7 +48,7 @@ class MetaLearnerMotivation:
         if self._variant == 'B':
             reward = torch.exp(k * torch.abs(error - error_estimate)) - 1
 
-        reward = torch.max(reward, self._forward_model.reward(error))
+        reward = torch.max(reward, self._forward_model.reward(error=error))
 
         return reward * eta
 