891ec70102246594313afbee4ba43ccfb9ec9bc0
pechac <higsik@gmail.com>
2020-10-15 18:09:24 +0200

    a2c entropy term

--- a2c_experiment.py
+++ a2c_experiment.py
@@ -1,6 +1,7 @@
 import numpy
 import torch
 from etaprogress.progress import ProgressBar
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
 
 
 class ExperimentA2C:
@@ -8,41 +9,49 @@ class ExperimentA2C:
         self._env_name = env_name
         self._env = env
         self._config = config
-        self._agent = None
 
     def run_baseline(self, agent, trial):
         config = self._config
-        bar = ProgressBar(config.episodes, max_width=40)
-        ext_rewards = numpy.zeros(config.episodes)
 
-        for e in range(config.episodes):
-            bar.numerator = e
-            # state0 = torch.tensor(self._env.reset(), dtype=torch.float32).unsqueeze(0).to(config.device)
-            # done = False
-            #
-            # if e % 100 == 0:
-            #     while not done:
-            #         self._env.render()
-            #         action0, _ = agent.get_action(state0)
-            #         next_state, reward, done, info = self._env.step(action0)
-            #         state0 = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0).to(config.device)
+        if config.load:
+            agent.load(config.load)
 
-            state0 = torch.tensor(self._env.reset(), dtype=torch.float32).unsqueeze(0).to(config.device)
-            done = False
-            total_reward = 0
-            train_steps = 0
+            for i in range(5):
+                video_path = 'a2c_{0}_{1}_{2:d}.mp4'.format(config.name, config.model, i)
+                # video_recorder = VideoRecorder(self._env, video_path, enabled=video_path is not None)
+                state0 = torch.tensor(self._env.reset(), dtype=torch.float32).unsqueeze(0).to(config.device)
+                done = False
 
-            while not done:
-                action0, log_prob = agent.get_action(state0)
-                next_state, reward, done, info = self._env.step(action0)
-                total_reward += reward
-                train_steps += 1
-                agent.train(state0, log_prob, reward, done)
-                state0 = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0).to(config.device)
+                while not done:
+                    self._env.render()
+                    # video_recorder.capture_frame()
+                    action0, _, _ = agent.get_action(state0)
+                    next_state, reward, done, info = self._env.step(action0)
+                    state0 = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0).to(config.device)
+                # video_recorder.close()
+        else:
+            bar = ProgressBar(config.episodes, max_width=40)
+            ext_rewards = numpy.zeros(config.episodes)
 
-            ext_rewards[e] = total_reward
-            print('Episode {0:d} training [ext. reward {1:f} steps {2:d}]'.format(e, total_reward, train_steps))
-            print(bar)
+            for e in range(config.episodes):
+                bar.numerator = e
 
-        agent.save('./models/{0:s}_{1}_{2:d}'.format(self._env_name, config.model, trial))
-        numpy.save('a2c_{0}_{1}_{2:d}_re'.format(config.name, config.model, trial), ext_rewards)
+                state0 = torch.tensor(self._env.reset(), dtype=torch.float32).unsqueeze(0).to(config.device)
+                done = False
+                total_reward = 0
+                train_steps = 0
+
+                while not done:
+                    action0, prob, log_prob = agent.get_action(state0)
+                    next_state, reward, done, info = self._env.step(action0)
+                    total_reward += reward
+                    train_steps += 1
+                    agent.train(state0, prob, log_prob, reward, done)
+                    state0 = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0).to(config.device)
+
+                ext_rewards[e] = total_reward
+                print('Episode {0:d} training [ext. reward {1:f} steps {2:d}]'.format(e, total_reward, train_steps))
+                print(bar)
+
+            agent.save('./models/{0:s}_{1}_{2:d}'.format(self._env_name, config.model, trial))
+            numpy.save('a2c_{0}_{1}_{2:d}_re'.format(config.name, config.model, trial), ext_rewards)
--- algorithms/A2C.py
+++ algorithms/A2C.py
@@ -6,6 +6,7 @@ class A2C:
     def __init__(self, agent, lr, actor_loss_weight, critic_loss_weight, gamma, batch_size, weight_decay=0, device='cpu'):
         self._agent = agent
         self._optimizer = torch.optim.Adam(self._agent.parameters(), lr=lr, weight_decay=weight_decay)
+        self._beta = 0.1
         self._gamma = gamma
         self._device = device
         self._batch_size = batch_size
@@ -15,14 +16,16 @@ class A2C:
         self._trajectory = []
         self._rewards = []
 
-    def train(self, state0, log_prob, reward, done):
+    def train(self, state0, prob, log_prob, reward, done):
         state_value = self._agent.value(state0)
-        self._trajectory.append((log_prob, state_value))
+
+        self._trajectory.append((prob, log_prob, state_value))
         self._rewards.append(reward)
 
         if done or len(self._trajectory) == self._batch_size:
             R = 0
             policy_loss = []
+            entropy_loss = []
             value_loss = []
             returns = []
 
@@ -35,15 +38,17 @@ class A2C:
             if len(self._trajectory) > 1:
                 returns = (returns - returns.mean()) / (returns.std() + 1e-8)
 
-            for (log_prob, value), R in zip(self._trajectory, returns):
+            for (prob, log_prob, value), R in zip(self._trajectory, returns):
                 advantage = R - value.item()
                 policy_loss.append(-log_prob * advantage)
+                entropy_loss.append(prob * log_prob)
                 value_loss.append(torch.nn.functional.smooth_l1_loss(value, R.unsqueeze(0)))
 
             self._optimizer.zero_grad()
+            entropy_loss = torch.stack(entropy_loss).sum(dim=1).mean()
             policy_loss = torch.stack(policy_loss).sum()
             value_loss = torch.stack(value_loss).sum()
-            loss = policy_loss * self._actor_loss_weight + value_loss * self._critic_loss_weight
+            loss = policy_loss * self._actor_loss_weight + self._beta * entropy_loss + value_loss * self._critic_loss_weight
             loss.backward()
             self._optimizer.step()
 
@@ -55,7 +60,7 @@ class A2C:
         m = Categorical(probs)
         action = m.sample()
 
-        return action.item(), m.log_prob(action)
+        return action.item(), probs, m.log_prob(action)
 
     def get_probs(self, state):
         probs = torch.softmax(self._agent.action(state), dim=-1)
@@ -64,3 +69,6 @@ class A2C:
 
     def save(self, path):
         torch.save(self._agent.state_dict(), path + '.pth')
+
+    def load(self, path):
+        self._agent.load_state_dict(torch.load(path + '.pth'))
