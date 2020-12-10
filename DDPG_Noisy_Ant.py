3ca61d9f19de4b40b59aabae6fd5c7900432476b
pechac <higsik@gmail.com>
2020-12-10 22:34:51 +0100

    noisy experiment fix

--- DDPG_Noisy_Ant.py
+++ DDPG_Noisy_Ant.py
@@ -1,9 +1,12 @@
 import gym
+import pybullet_envs
 import torch
+from torch import nn
 
 from algorithms.DDPG import DDPGCritic, DDPGActor, DDPG
 from algorithms.ReplayBuffer import ExperienceReplayBuffer
-from ddpg_experiment import ExperimentDDPG
+from ddpg_noisy_experiment import ExperimentNoisyDDPG
+from modules.NoisyLinear import NoisyLinear
 from motivation.ForwardModelMotivation import ForwardModel, ForwardModelMotivation
 from motivation.M3Motivation import M3Motivation
 from motivation.MateLearnerMotivation import MetaLearnerModel, MetaLearnerMotivation
@@ -13,9 +16,9 @@ class Critic(DDPGCritic):
     def __init__(self, state_dim, action_dim, config):
         super(Critic, self).__init__(state_dim, action_dim)
 
-        self._hidden0 = torch.nn.Linear(state_dim, config.critic.h1)
-        self._hidden1 = torch.nn.Linear(config.critic.h1 + action_dim, config.critic.h2)
-        self._output = torch.nn.Linear(config.critic.h2, 1)
+        self._hidden0 = nn.Linear(state_dim, config.critic.h1)
+        self._hidden1 = nn.Linear(config.critic.h1 + action_dim, config.critic.h2)
+        self._output = nn.Linear(config.critic.h2, 1)
 
         self.init()
 
@@ -28,18 +31,18 @@ class Critic(DDPGCritic):
         return value
 
     def init(self):
-        torch.nn.init.xavier_uniform_(self._hidden0.weight)
-        torch.nn.init.xavier_uniform_(self._hidden1.weight)
-        torch.nn.init.uniform_(self._output.weight, -3e-3, 3e-3)
+        nn.init.xavier_uniform_(self._hidden0.weight)
+        nn.init.xavier_uniform_(self._hidden1.weight)
+        nn.init.uniform_(self._output.weight, -3e-3, 3e-3)
 
 
 class Actor(DDPGActor):
     def __init__(self, state_dim, action_dim, config):
         super(Actor, self).__init__(state_dim, action_dim)
 
-        self._hidden0 = torch.nn.Linear(state_dim, config.actor.h1)
-        self._hidden1 = torch.nn.Linear(config.actor.h1, config.actor.h2)
-        self._output = torch.nn.Linear(config.actor.h2, action_dim)
+        self._hidden0 = nn.Linear(state_dim, config.actor.h1)
+        self._hidden1 = nn.Linear(config.actor.h1, config.actor.h2)
+        self._output = NoisyLinear(config.actor.h2, action_dim)
 
         self.init()
 
@@ -51,61 +54,63 @@ class Actor(DDPGActor):
         return policy
 
     def init(self):
-        torch.nn.init.xavier_uniform_(self._hidden0.weight)
-        torch.nn.init.xavier_uniform_(self._hidden1.weight)
-        torch.nn.init.uniform_(self._output.weight, -3e-1, 3e-1)
+        nn.init.xavier_uniform_(self._hidden0.weight)
+        nn.init.xavier_uniform_(self._hidden1.weight)
 
 
 class ForwardModelNetwork(ForwardModel):
     def __init__(self, state_dim, action_dim, config):
         super(ForwardModelNetwork, self).__init__(state_dim, action_dim, config)
-        self._hidden0 = torch.nn.Linear(state_dim + action_dim, config.forward_model.h1)
-        self._hidden1 = torch.nn.Linear(config.forward_model.h1, config.forward_model.h2)
-        self._output = torch.nn.Linear(config.forward_model.h2, state_dim)
+        self._action = nn.Linear(action_dim, state_dim)
+        self._hidden0 = nn.Linear(state_dim + state_dim, config.forward_model.h1)
+        self._hidden1 = nn.Linear(config.forward_model.h1, config.forward_model.h2)
+        self._output = nn.Linear(config.forward_model.h2, state_dim)
         self.init()
 
     def forward(self, state, action):
-        x = torch.cat([state, action], state.ndim - 1)
+        x = torch.relu(self._action(action))
+        x = torch.cat([state, x], state.ndim - 1)
         x = torch.relu(self._hidden0(x))
         x = torch.relu(self._hidden1(x))
         value = self._output(x)
         return value
 
     def init(self):
-        torch.nn.init.xavier_uniform_(self._hidden0.weight)
-        torch.nn.init.xavier_uniform_(self._hidden1.weight)
-        torch.nn.init.uniform_(self._output.weight, -3e-1, 3e-1)
+        nn.init.xavier_uniform_(self._action.weight)
+        nn.init.xavier_uniform_(self._hidden0.weight)
+        nn.init.xavier_uniform_(self._hidden1.weight)
+        nn.init.uniform_(self._output.weight, -3e-3, 3e-3)
 
 
 class MetaLearnerNetwork(MetaLearnerModel):
     def __init__(self, state_dim, action_dim, config):
         super(MetaLearnerNetwork, self).__init__(state_dim, action_dim, config)
-        self._hidden0 = torch.nn.Linear(state_dim + action_dim, config.metacritic.h1)
-        self._hidden1 = torch.nn.Linear(config.metacritic.h1, config.metacritic.h2)
-        self._output = torch.nn.Linear(config.metacritic.h2, 1)
+        self._hidden0 = nn.Linear(state_dim + action_dim, config.metacritic.h1)
+        self._hidden1 = nn.Linear(config.metacritic.h1, config.metacritic.h2)
+        self._output = nn.Linear(config.metacritic.h2, 1)
 
         self.init()
 
     def forward(self, state, action):
         x = torch.cat([state, action], state.ndim - 1)
-        x = torch.nn.functional.relu(self._hidden0(x))
-        x = torch.nn.functional.relu(self._hidden1(x))
+        x = nn.functional.relu(self._hidden0(x))
+        x = nn.functional.relu(self._hidden1(x))
         value = self._output(x)
         return value
 
     def init(self):
-        torch.nn.init.xavier_uniform_(self._hidden0.weight)
-        torch.nn.init.xavier_uniform_(self._hidden1.weight)
-        torch.nn.init.uniform_(self._output.weight, -3e-1, 3e-1)
+        nn.init.xavier_uniform_(self._hidden0.weight)
+        nn.init.xavier_uniform_(self._hidden1.weight)
+        nn.init.uniform_(self._output.weight, -3e-1, 3e-1)
 
 
-class M3Gate(torch.nn.Module):
+class M3Gate(nn.Module):
     def __init__(self, state_dim, im_dim, config):
         super(M3Gate, self).__init__()
 
-        self._hidden0 = torch.nn.Linear(state_dim, config.m3gate.h1)
-        self._hidden1 = torch.nn.Linear(config.m3gate.h1, config.m3gate.h2)
-        self._output = torch.nn.Linear(config.m3gate.h2, im_dim)
+        self._hidden0 = nn.Linear(state_dim, config.m3gate.h1)
+        self._hidden1 = nn.Linear(config.m3gate.h1, config.m3gate.h2)
+        self._output = nn.Linear(config.m3gate.h2, im_dim)
 
         self.init()
 
@@ -117,17 +122,17 @@ class M3Gate(torch.nn.Module):
         return value
 
     def init(self):
-        torch.nn.init.xavier_uniform_(self._hidden0.weight)
-        torch.nn.init.xavier_uniform_(self._hidden1.weight)
-        torch.nn.init.uniform_(self._output.weight, -3e-1, 3e-1)
+        nn.init.xavier_uniform_(self._hidden0.weight)
+        nn.init.xavier_uniform_(self._hidden1.weight)
+        nn.init.uniform_(self._output.weight, -3e-1, 3e-1)
 
 
-class M3Critic(torch.nn.Module):
+class M3Critic(nn.Module):
     def __init__(self, state_dim, im_dim, config):
         super(M3Critic, self).__init__()
-        self._hidden0 = torch.nn.Linear(state_dim, config.m3critic.h1)
-        self._hidden1 = torch.nn.Linear(config.m3critic.h1 + im_dim, config.m3critic.h2)
-        self._output = torch.nn.Linear(config.m3critic.h2, 1)
+        self._hidden0 = nn.Linear(state_dim, config.m3critic.h1)
+        self._hidden1 = nn.Linear(config.m3critic.h1 + im_dim, config.m3critic.h2)
+        self._output = nn.Linear(config.m3critic.h2, 1)
         self.init()
 
     def forward(self, state, action):
@@ -139,9 +144,9 @@ class M3Critic(torch.nn.Module):
         return value
 
     def init(self):
-        torch.nn.init.xavier_uniform_(self._hidden0.weight)
-        torch.nn.init.xavier_uniform_(self._hidden1.weight)
-        torch.nn.init.uniform_(self._output.weight, -3e-1, 3e-1)
+        nn.init.xavier_uniform_(self._hidden0.weight)
+        nn.init.xavier_uniform_(self._hidden1.weight)
+        nn.init.uniform_(self._output.weight, -3e-1, 3e-1)
 
 
 def run_baseline(config):
@@ -149,7 +154,7 @@ def run_baseline(config):
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
 
-    experiment = ExperimentDDPG('AntBulletEnv-v0', env, config)
+    experiment = ExperimentNoisyDDPG('AntBulletEnv-v0', env, config)
 
     for i in range(config.trials):
         actor = Actor(state_dim, action_dim, config)
@@ -166,7 +171,7 @@ def run_forward_model(config):
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
 
-    experiment = ExperimentDDPG('AntBulletEnv-v0', env, config)
+    experiment = ExperimentNoisyDDPG('AntBulletEnv-v0', env, config)
 
     for i in range(config.trials):
         actor = Actor(state_dim, action_dim, config)
@@ -177,9 +182,11 @@ def run_forward_model(config):
 
         if config.forward_model.get('batch_size') is not None:
             forward_model = ForwardModelMotivation(ForwardModelNetwork(state_dim, action_dim, config), config.forward_model.lr, config.forward_model.eta,
+                                                   config.forward_model.variant, env._max_episode_steps * 10,
                                                    memory, config.forward_model.batch_size)
         else:
-            forward_model = ForwardModelMotivation(ForwardModelNetwork(state_dim, action_dim, config), config.forward_model.lr, config.forward_model.eta)
+            forward_model = ForwardModelMotivation(ForwardModelNetwork(state_dim, action_dim, config), config.forward_model.lr, config.forward_model.eta,
+                                                   config.forward_model.variant, env._max_episode_steps * 10)
 
         agent.add_motivation_module(forward_model)
 
@@ -198,7 +205,7 @@ def run_surprise_model(args):
     args.eta = 1
     args.metacritic_variant = 'C'
 
-    experiment = ExperimentDDPG('HalfCheetahPyBulletEnv-v0', Actor, Critic)
+    experiment = ExperimentNoisyDDPG('AntBulletEnv-v0', Actor, Critic)
     experiment.run_metalearner_model(args)
 
 
@@ -207,7 +214,7 @@ def run_metalearner_model(config):
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
 
-    experiment = ExperimentDDPG('AntBulletEnv-v0', env, config)
+    experiment = ExperimentNoisyDDPG('AntBulletEnv-v0', env, config)
 
     for i in range(config.trials):
         actor = Actor(state_dim, action_dim, config)
@@ -218,16 +225,19 @@ def run_metalearner_model(config):
 
         if config.forward_model.get('batch_size') is not None:
             forward_model = ForwardModelMotivation(ForwardModelNetwork(state_dim, action_dim, config), config.forward_model.lr, config.forward_model.eta,
+                                                   config.forward_model.variant, env._max_episode_steps * 10,
                                                    memory, config.forward_model.batch_size)
         else:
-            forward_model = ForwardModelMotivation(ForwardModelNetwork(state_dim, action_dim, config), config.forward_model.lr, config.forward_model.eta)
+            forward_model = ForwardModelMotivation(ForwardModelNetwork(state_dim, action_dim, config), config.forward_model.lr, config.forward_model.eta,
+                                                   config.forward_model.variant, env._max_episode_steps * 10)
 
         if config.metacritic.get('batch_size') is not None:
             metacritic = MetaLearnerMotivation(MetaLearnerNetwork(state_dim, action_dim, config), forward_model, config.metacritic.lr,
-                                               config.metacritic.variant, config.metacritic.eta, memory, config.metacritic.batch_size)
+                                               config.metacritic.variant, env._max_episode_steps * 10, config.metacritic.eta,
+                                               memory, config.metacritic.batch_size)
         else:
             metacritic = MetaLearnerMotivation(MetaLearnerNetwork(state_dim, action_dim, config), forward_model, config.metacritic.lr,
-                                               config.metacritic.variant, config.metacritic.eta)
+                                               config.metacritic.variant, env._max_episode_steps * 10, config.metacritic.eta)
 
         agent.add_motivation_module(metacritic)
 
@@ -241,7 +251,7 @@ def run_m3_model(config):
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
 
-    experiment = ExperimentDDPG('AntBulletEnv-v0', env, config)
+    experiment = ExperimentNoisyDDPG('AntBulletEnv-v0', env, config)
 
     for i in range(config.trials):
         actor = Actor(state_dim, action_dim, config)
