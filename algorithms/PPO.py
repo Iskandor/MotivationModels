320388ec322b19974f2d049e45a418ab11daaece
pechac <higsik@gmail.com>
2021-04-17 11:43:57 +0200

    fix of critical error in multienv PPO version

--- algorithms/PPO.py
+++ algorithms/PPO.py
@@ -34,10 +34,15 @@ class PPO:
             self._train()
 
     def train_n_env(self, state0, value, action, prob, state1, reward, done):
+        trajectory_size_1_env = self._trajectory_size // self._n_env
+
+        if len(self._trajectories[0]) == trajectory_size_1_env - 1:
+            done = torch.zeros_like(done)
+
         for i in range(self._n_env):
             self._trajectories[i].append((state0[i].unsqueeze(0), value[i].unsqueeze(0), action[i].unsqueeze(0), prob[i].unsqueeze(0), state1[i].unsqueeze(0), reward[i], done[i]))
 
-        if len(self._trajectories[0]) == self._trajectory_size // self._n_env:
+        if len(self._trajectories[0]) == trajectory_size_1_env:
             for i in range(self._n_env):
                 self._trajectory += self._trajectories[i]
                 self._trajectories[i].clear()
