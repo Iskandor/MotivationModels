d2eb730abea92242445964fcf38e65ceacc2d5d8
pechac <higsik@gmail.com>
2021-08-18 20:43:25 +0200

    rnd conv model

--- modules/rnd_models/RNDModelAeris.py
+++ modules/rnd_models/RNDModelAeris.py
@@ -18,34 +18,36 @@ class RNDModelAeris(nn.Module):
         self.channels = input_shape[0]
         self.width = input_shape[1]
 
-        fc_count = 512
+        fc_count = config.forward_model_kernels_count * self.width // 4
         hidden_count = 64
 
         self.target_model = nn.Sequential(
-            nn.Linear(self.channels * self.width, fc_count),
+            nn.Conv1d(self.channels, config.forward_model_kernels_count, kernel_size=8, stride=4, padding=2),
             nn.ReLU(),
-            nn.Linear(fc_count, fc_count),
+            nn.Conv1d(config.forward_model_kernels_count, config.forward_model_kernels_count * 2, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Linear(fc_count, fc_count),
+            nn.Conv1d(config.forward_model_kernels_count * 2, config.forward_model_kernels_count * 2, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.Flatten(),
             nn.Linear(fc_count, hidden_count)
         )
 
         init_orthogonal(self.target_model[0], np.sqrt(2))
         init_orthogonal(self.target_model[2], np.sqrt(2))
         init_orthogonal(self.target_model[4], np.sqrt(2))
-        init_orthogonal(self.target_model[6], 1)
+        init_orthogonal(self.target_model[7], 1)
 
         for param in self.target_model.parameters():
             param.requires_grad = False
 
         self.model = nn.Sequential(
-            nn.Linear(self.channels * self.width, fc_count),
+            nn.Conv1d(self.channels, config.forward_model_kernels_count, kernel_size=8, stride=4, padding=2),
             nn.ReLU(),
-            nn.Linear(fc_count, fc_count),
+            nn.Conv1d(config.forward_model_kernels_count, config.forward_model_kernels_count * 2, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Linear(fc_count, fc_count),
+            nn.Conv1d(config.forward_model_kernels_count * 2, config.forward_model_kernels_count * 2, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.Flatten(),
             nn.Linear(fc_count, hidden_count),
             nn.ReLU(),
             nn.Linear(hidden_count, hidden_count),
@@ -56,18 +58,20 @@ class RNDModelAeris(nn.Module):
         init_orthogonal(self.model[0], np.sqrt(2))
         init_orthogonal(self.model[2], np.sqrt(2))
         init_orthogonal(self.model[4], np.sqrt(2))
-        init_orthogonal(self.model[6], 0.1)
-        init_orthogonal(self.model[8], 0.1)
-        init_orthogonal(self.model[10], 0.01)
+        init_orthogonal(self.model[7], 0.1)
+        init_orthogonal(self.model[9], 0.1)
+        init_orthogonal(self.model[11], 0.01)
 
     def forward(self, state):
         x = state - self.state_average.expand(state.shape[0], *state.shape[1:])
-        predicted_code = self.model(x.view(-1, self.channels * self.width))
+        # x.view(-1, self.channels * self.width)
+        predicted_code = self.model(x)
         return predicted_code
 
     def encode(self, state):
         x = state - self.state_average.expand(state.shape[0], *state.shape[1:])
-        return self.target_model(x.view(-1, self.channels * self.width))
+        # x.view(-1, self.channels * self.width)
+        return self.target_model(x)
 
     def error(self, state):
         with torch.no_grad():
@@ -105,34 +109,36 @@ class QRNDModelAeris(nn.Module):
 
         self.state_average = torch.zeros((1, input_shape[0], input_shape[1]))
 
-        fc_count = 512
+        fc_count = config.forward_model_kernels_count * self.width // 4
         hidden_count = 64
 
         self.target_model = nn.Sequential(
-            nn.Linear(self.channels * self.width, fc_count),
+            nn.Conv1d(self.channels, config.forward_model_kernels_count, kernel_size=8, stride=4, padding=2),
             nn.ReLU(),
-            nn.Linear(fc_count, fc_count),
+            nn.Conv1d(config.forward_model_kernels_count, config.forward_model_kernels_count * 2, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Linear(fc_count, fc_count),
+            nn.Conv1d(config.forward_model_kernels_count * 2, config.forward_model_kernels_count * 2, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.Flatten(),
             nn.Linear(fc_count, hidden_count)
         )
 
         init_orthogonal(self.target_model[0], np.sqrt(2))
         init_orthogonal(self.target_model[2], np.sqrt(2))
         init_orthogonal(self.target_model[4], np.sqrt(2))
-        init_orthogonal(self.target_model[6], 1)
+        init_orthogonal(self.target_model[7], 1)
 
         for param in self.target_model.parameters():
             param.requires_grad = False
 
         self.model = nn.Sequential(
-            nn.Linear(self.channels * self.width, fc_count),
+            nn.Conv1d(self.channels, config.forward_model_kernels_count, kernel_size=8, stride=4, padding=2),
             nn.ReLU(),
-            nn.Linear(fc_count, fc_count),
+            nn.Conv1d(config.forward_model_kernels_count, config.forward_model_kernels_count * 2, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Linear(fc_count, fc_count),
+            nn.Conv1d(config.forward_model_kernels_count * 2, config.forward_model_kernels_count * 2, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.Flatten(),
             nn.Linear(fc_count, hidden_count),
             nn.ReLU(),
             nn.Linear(hidden_count, hidden_count),
@@ -143,22 +149,24 @@ class QRNDModelAeris(nn.Module):
         init_orthogonal(self.model[0], np.sqrt(2))
         init_orthogonal(self.model[2], np.sqrt(2))
         init_orthogonal(self.model[4], np.sqrt(2))
-        init_orthogonal(self.model[6], 0.1)
-        init_orthogonal(self.model[8], 0.1)
-        init_orthogonal(self.model[10], 0.01)
+        init_orthogonal(self.model[7], 0.1)
+        init_orthogonal(self.model[9], 0.1)
+        init_orthogonal(self.model[11], 0.01)
 
     def forward(self, state, action):
         x = state - self.state_average.expand(state.shape[0], *state.shape[1:])
         a = action.unsqueeze(2).repeat(1, 1, state.shape[2])
         x = torch.cat([x, a], dim=1)
-        predicted_code = self.model(x.view(-1, self.channels * self.width))
+        predicted_code = self.model(x)
+        # x.view(-1, self.channels * self.width)
         return predicted_code
 
     def encode(self, state, action):
         x = state - self.state_average.expand(state.shape[0], *state.shape[1:])
         a = action.unsqueeze(2).repeat(1, 1, state.shape[2])
         x = torch.cat([x, a], dim=1)
-        return self.target_model(x.view(-1, self.channels * self.width))
+        # x.view(-1, self.channels * self.width)
+        return self.target_model(x)
 
     def error(self, state, action):
         prediction = self(state, action)
