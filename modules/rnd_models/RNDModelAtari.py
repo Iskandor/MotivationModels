78d5e820df98aca76432a3766dcc27128bfbdfac
mpechac <higsik@gmail.com>
2022-04-19 10:28:00 +0200

    CND loss coefficient in config 2

--- modules/rnd_models/RNDModelAtari.py
+++ modules/rnd_models/RNDModelAtari.py
@@ -176,7 +176,7 @@ class CNDModelAtari(nn.Module):
         analytic = CNDAnalytic()
         analytic.update(loss_prediction=loss_prediction.unsqueeze(-1).detach(), loss_target=loss_target.unsqueeze(-1).detach())
 
-        return loss_prediction + loss_target * 10
+        return loss_prediction * self.config.cnd_loss_pred + loss_target * self.config.cnd_loss_target
 
     @staticmethod
     def k_distance(k, prediction, target, reduction='sum'):
