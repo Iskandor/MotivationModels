7e56473f1e9b37a7c1e46dabaec5484f293c40ef
mpechac <higsik@gmail.com>
2022-09-26 22:07:11 +0200

    analysis update

--- analysis.py
+++ analysis.py
@@ -1,12 +1,14 @@
 import os
 
+from pathlib import Path
+
 from agents.PPOAtariAgent import PPOAtariCNDAgent, PPOAtariRNDAgent, PPOAtariFEDRefAgent
 from analytic.FeatureAnalysis import FeatureAnalysis
 from analytic.MetricTensor import initialize
 from analytic.StateCollector import collect_states, collect_samples
+from plots.paths import models_root, states_root
 
 if __name__ == '__main__':
-    models_path = 'C:/Git/Experiments/models'
     results_path = 'C:/Git/Experiments/data/ppo'
 
     # agent, env, _ = initialize('./models/montezuma_21_cnd_0', '21', PPOAtariCNDAgent)
@@ -19,10 +21,11 @@ if __name__ == '__main__':
 
     config = []
 
-    for i in range(6):
-        # agent, _, _ = initialize(os.path.join(models_path, 'montezuma_21_cnd_{0:d}'.format(i)), '26', PPOAtariCNDAgent)
-        # collect_samples(agent, './states.npy', './cnd21_{0:d}'.format(i), 'cnd')
-        config.append({'samples': './cnd21_{0:d}.npy'.format(i), 'results': os.path.join(results_path, 'cnd/montezuma/21B/ppo_montezuma_21_cnd_{0:d}.npy'.format(i)), 'label': 'cnd21_{0:d}'.format(i)})
+    config_id = 40
+    for i in range(8):
+        # agent, _, _ = initialize(os.path.join(models_root, 'montezuma_{0:d}_cnd_{1:d}'.format(config_id, i)), '{0:d}'.format(config_id), PPOAtariCNDAgent)
+        # collect_samples(agent, Path(states_root) / 'states.npy', Path(states_root) / 'cnd{0:d}_{1:d}'.format(config_id, i), 'cnd')
+        config.append({'samples': Path(states_root) / 'cnd{0:d}_{1:d}.npy'.format(config_id, i), 'results': os.path.join(results_path, 'cnd/montezuma/{0:d}/ppo_montezuma_{1:d}_cnd_{2:d}.npy'.format(config_id, config_id, i)), 'label': 'cnd{0:d}_{1:d}'.format(config_id,i)})
 
     # config = [
     #     {'samples': './fed_ref.npy', 'results': os.path.join(results_path, 'fed_ref/montezuma/23/ppo_montezuma_23_fed_ref_0.npy'), 'label': 'fed_ref'},
@@ -34,4 +37,5 @@ if __name__ == '__main__':
     # ]
 
     analysis = FeatureAnalysis(config)
-    analysis.plot()
+    analysis.plot('features_cnd{0:d}'.format(config_id))
+    analysis.plot_feature_boxplot('features_cnd{0:d}_fd1'.format(config_id))
--- analytic/FeatureAnalysis.py
+++ analytic/FeatureAnalysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
+from tqdm import tqdm
 
 
 class FeatureAnalysis:
@@ -14,7 +15,7 @@ class FeatureAnalysis:
             self.results.append(np.load(row['results'], allow_pickle=True).item())
             self.labels.append(row['label'])
 
-    def plot(self):
+    def plot(self, filename):
         data = {'max': [], 'diff_mean': [], 'diff_std': [], 'feature': []}
         for r in self.results:
             data['max'].append(r['re']['sum'].max())
@@ -33,7 +34,26 @@ class FeatureAnalysis:
         self.pca(data['feature'])
         fig.add_subplot(313)
         self.max_reward(data['max'])
-        plt.savefig('./feature_analysis.png')
+        plt.savefig('./{0:s}.png'.format(filename))
+
+    def plot_feature_boxplot(self, filename):
+        fig = plt.figure(figsize=(40.96, len(self.data) * 2.56))
+        plt.tight_layout()
+
+        flierprops = dict(marker='o', markerfacecolor='green', markersize=2, linestyle='none')
+
+        for i in tqdm(range(len(self.data)), total=len(self.data)):
+            f = self.data[i]['feature']
+            dim = f.shape[1]
+            pca = PCA()
+            pca.fit(f)
+            y = np.squeeze(pca.transform(np.ones((1, dim))))
+            b = np.flip(np.abs(y).argsort())
+            f = f[:, b]
+
+            plt.subplot2grid(shape=(len(self.data), 1), loc=(i, 0))
+            plt.boxplot(f, flierprops=flierprops, showfliers=False)
+        plt.savefig('./{0:s}.png'.format(filename))
 
     def mean_std(self, diff_mean, diff_std):
         plt.gca().set_title('Std. deviation of feature difference')
@@ -52,18 +72,19 @@ class FeatureAnalysis:
             plt.legend()
 
     def pca(self, features):
-        plt.gca().set_title('Singular values')
-        plt.xlabel('singular value index')
+        plt.gca().set_title('Eigenvalues')
+        plt.xlabel('eigenvalue index')
         plt.ylabel('value')
-        # plt.yscale('log')
+        plt.yscale('log')
         plt.grid()
 
         for i, f in enumerate(features):
             pca = PCA()
-            pca.fit(f - f.mean())
+            pca.fit(f)
+
             x_axis = np.arange(len(pca.singular_values_))
 
-            plt.plot(x_axis, pca.singular_values_, color=self.colors[i], label=self.labels[i])
+            plt.plot(x_axis, pca.singular_values_ ** 2, color=self.colors[i], label=self.labels[i])
 
         plt.legend()
 
@@ -79,3 +100,7 @@ class FeatureAnalysis:
         for i, r in enumerate(rewards):
             plt.bar(i * 0.5, height=r, width=0.3, color=self.colors[i], label=self.labels[i])
         plt.legend()
+
+    def dim_boxplot(self):
+        plt.gca().set_title('Feature dimension statistics')
+        plt.boxplot(self.data)
